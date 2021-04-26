1065236155de6377f2e62889cda9a68c327f4177
Cash Costello <cash.costello@jhuapl.edu>
2021-04-26 17:28:11 -0400

    make the normalize code installable with pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("readme.txt", "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="scripts_normalization",
+    version="4.0",
+    author="Ramy Eskander",
+    author_email="rnd2110@columbia.edu",
+    description="A library for text normalization for a set of languages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rnd2110/SCRIPTS_Normalization",
+    packages=["scripts_normalization"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
